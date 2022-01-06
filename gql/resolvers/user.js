f4b7e0b5f7f37455114191f9f289b5b1b577de21
radiylon <radiylon@gmail.com>
2022-01-06 03:38:30 -0800

    Validate registration against existing usernames

--- gql/resolvers/user.js
+++ gql/resolvers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 const User = require('../../models/user');
 const config = require('../../config');
 
@@ -19,9 +20,21 @@ const resolvers = {
     registerUser: async (_, { input }, context) => {
       const { username, email, password, confirmPassword } = input;
 
+      // validate for unique username
+      const existingUser = await User.findOne({ username: username });
+      console.log('existingUser', existingUser);
+      if (existingUser) {
+        // UserInputError for apollo server-side validation
+        throw new UserInputError('Username already taken', {
+          errors: {
+            username: 'This username is already taken',
+          }
+        });
+      }
+
       // hash password before storing
       const hashedPassword = await bcrypt.hash(password, 12);
-      
+
       // generate newUser based on input
       const newUser = new User({
         username,
